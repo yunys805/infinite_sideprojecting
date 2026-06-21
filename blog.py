dc5065e76ba4c2de26db64d594442b5e3bf9e484
yunys805 <yunys805@gmail.com>
2026-06-22 00:33:35 +0900

    feat : 글꼴 추가 - 나눔고딕

--- blog.py
+++ blog.py
@@ -12,6 +12,8 @@ with col2:
 
     st.markdown("""
         <style>
+        @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700&display=swap');
+
         /* =============== 1. streamlit 사전 설정 변경 =============== */
         /* 1. 창 크기 조절기 삭제 */
         textarea {
@@ -29,60 +31,58 @@ with col2:
         }
 
         /* =============== 2. 제목·본문 창 설정 =============== */
-        /* 1. 제목 창 설정 */
-        .st-key-title textarea {
-            font-size: 32px !important;
-            line-height: 1.6 !important;
-            margin-left: 15px;
-            margin-right: 15px;
+        /* 1. 제목·본문 창 공통 설정 */
+        .st-key-title textarea,
+        .st-key-body textarea {
+            margin-left: 40px;
+            margin-right: 40px;
             overflow: hidden;
+            
+            font-family: 'Nanum Gothic', sans-serif !important;
+            color: black !important;
+            letter-spacing: -0.05em !important;
+            word-spacing: 0.1em !important;
+            line-height: 1.7 !important;
+        }
+        div[data-baseweb="textarea"] {
+            border: none !important;
+        }
+
+        /* 2. 제목 창 단독 설정 */
+        .st-key-title textarea {
+            font-size: 35px !important;
         }
                 
-        /* 2. 본문 창 설정 */
+        /* 3. 본문 창 단독 설정 */
         .st-key-body textarea {
             font-size: 24px !important;
-            line-height: 1.6 !important;
-            margin-left: 15px;
-            margin-right: 15px;
-            overflow: hidden;
         }
         
-        /* 3. 가로선 위·아래 간격 설정 */
+        /* 4. 가로선 위·아래·좌우 간격 설정 */
         hr {
             margin: 0px !important;
-            width: 92% !important;
+            width: 87% !important;
             margin-left: auto !important;
             margin-right: auto !important;
         }  
         
-        /* 4. 제목·본문·가로선 전체 창 설정 */
+        /* 5. 제목·본문·가로선 전체 박스 설정 */
         div[data-testid="stColumn"]:nth-child(2) {
             background-color: white;
             padding-top: 30px;
             padding-left: 50px;
             padding-right: 50px;
+            border: 1px solid #ddd !important;
             border-radius: 10px !important
         }
 
-        /* 5. 제목·본문 색깔 설정 */
+        /* 6. 제목·본문 창 색깔 설정 */
         div[data-baseweb="base-input"] {
             background-color: white !important;
         }
 
-        .stTextArea > div {
-            border: none !important;
-        }
-
         </style>
+                
         """, unsafe_allow_html=True)
-
-    # 3. 철자 오류 밑줄 삭제
-    js = """
-        <script>
-            const txt_areas = window.parent.document.querySelectorAll('textarea');
-            txt_areas.forEach(txt => {
-                txt.spellcheck = false;
-            });
-        </script>
-        """
-    st.html(js)
+    
+  
